d7befb49581b72d6e8a16433380b064944e4af77
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-07 14:48:54 +0300

    Initiated the useSelector hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {useSelector} from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter); 
+
   return (
     <div>
       <h1>Counter App</h1>
