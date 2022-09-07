bd78c247f8535e5874f979321225c3e67bfa87b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-07 14:43:16 +0300

    Added an import for store and passed it as a parameter to the Provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store="./store/index.js">
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
