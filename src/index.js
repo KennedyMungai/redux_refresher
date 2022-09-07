3a93ed73f337bdc0c8e14e7e813a15824fa85c7a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-07 14:41:00 +0300

    Implemented the store and passed in the redux store as a parameter

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store="./store/index.js">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
