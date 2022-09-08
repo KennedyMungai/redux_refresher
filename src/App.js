8b33fbe6f90b961cf83d5250bc6a746a526dff6b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 09:02:46 +0300

    Creatd the increment and decrement arrow functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import {useSelector} from 'react-redux';
 function App() {
   const counter = useSelector((state) => state.counter); 
 
+  const increment = () => {
+
+  };
+
+  const decrement = () => {
+
+  };
+
   return (
     <div>
       <h1>Counter App</h1>
