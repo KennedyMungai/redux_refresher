474ebc6f41e09fd4973852cdd78f0b6eb02f8fae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-08 09:18:11 +0300

    Updated the variable name

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createStore } from "react-redux";
 
 const reducerFn = (state = {counter: 0}, action) => {
     if (action.type==="INC") {
-        return {state: state.counter++};
+        return {counter: state.counter++};
     }
 
     return state;                                                                                                                     
